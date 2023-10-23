f06e7343e66c8991add671fa4996f3c93560d292
isaiah <isaiahsinnathamby@gmail.com>
2023-10-23 04:53:58 -0400

    checkboxes + Fetch Dogs button retreive dogs - will work on temp css and then work on the pagination

--- src/components/FilterPage/index.js
+++ src/components/FilterPage/index.js
@@ -9,28 +9,72 @@ export default function FilterPage() {
     const [resultIds, setResultIds] = useState([]);
     const [dogDetails, setDogDetails] = useState([]);
 
-    const fetchData = () => {
-        // Fetch resultIds
-        axios.get('https://frontend-take-home-service.fetch.com/dogs/search?breeds=Beagle', { withCredentials: true })
+    const [availableBreeds, setAvailableBreeds] = useState([]);
+    const [selectedBreeds, setSelectedBreeds] = useState(new Set());
+
+    useEffect(() => {
+        axios.get('https://frontend-take-home-service.fetch.com/dogs/breeds', { withCredentials: true })
             .then(response => {
-                console.log('to see what happens in the .then');
-                console.log('DATA RESULT IDS', response.data.resultIds);
+                setAvailableBreeds(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching available breeds:', error);
+            });
+    }, []);
+
+    const handleCheckboxChange = (breed) => {
+        const newSelectedBreeds = new Set(selectedBreeds);
+        if (newSelectedBreeds.has(breed)) {
+            newSelectedBreeds.delete(breed);
+        } else {
+            newSelectedBreeds.add(breed);
+        }
+        setSelectedBreeds(newSelectedBreeds);
+    };
+
+    const fetchData = () => {
+        console.log('SELECTED BREEDS', selectedBreeds)
+
+        const quickTest = Array.from(selectedBreeds)
+        console.log('QUICK TEST', quickTest);
+
+        const breedParams = Array.from(selectedBreeds)
+            .map(breed => `breeds=${encodeURIComponent(breed)}`)
+            .join('&');
+        const url = `https://frontend-take-home-service.fetch.com/dogs/search?${breedParams}`;
+        console.log('THIS IS THE URL', url);
+
+        axios.get(url, { withCredentials: true })
+            .then(response => {
+                console.log('RESPONSE DATAAAAAAAA', response.data)
                 setResultIds(response.data.resultIds);
             })
             .catch(error => {
-                console.error('Error fetching resultIds:', error);
-                console.error('Error Details:', error.response);
+                console.error('Error fetching available breeds:', error);
             });
-    }
+    };
 
-    // console.log('resultIds', resultIds);
+    // const fetchData = () => {
+    //     // Fetch resultIds
+    //     axios.get('https://frontend-take-home-service.fetch.com/dogs/search?breeds=Beagle', { withCredentials: true })
+    //         .then(response => {
+    //             console.log('to see what happens in the .then');
+    //             console.log('DATA RESULT IDS', response.data.resultIds);
+    //             setResultIds(response.data.resultIds);
+    //         })
+    //         .catch(error => {
+    //             console.error('Error fetching resultIds:', error);
+    //             console.error('Error Details:', error.response);
+    //         });
+    // }
 
     useEffect(() => {
         if (resultIds.length > 0) {
             axios.post('https://frontend-take-home-service.fetch.com/dogs', resultIds, { withCredentials: true })
                 .then(response => {
-                    console.log('RESPONSE DATA', response.data)
                     setDogDetails(response.data);
+                    console.log('RESPONSE DATA', response.data)
+                    console.log('DOG DETAILS', dogDetails);
                 })
                 .catch(error => {
                     console.error('Error fetching dog details:', error);
@@ -55,31 +99,34 @@ export default function FilterPage() {
             <div className="filter-page-content">
                 <aside className="filter-sidebar">
                     <h2>Filters</h2>
-                    {/* Will include your filter components here */}
-                    {/* Will pass handleFilterChange to update filters */}
+                    {availableBreeds.map((breed, index) => (
+                        <label key={index}>
+                            <input
+                                type="checkbox"
+                                value={breed}
+                                onChange={() => handleCheckboxChange(breed)}
+                            />
+                            {breed}
+                        </label>
+                    ))}
                 </aside>
 
                 <main className="filter-results">
                     <h2>Results</h2>
-                    {/* Map over results and render pet cards or components */}
-                    {results.map((result, index) => (
-                        <div key={index}>
-                            {/* Individual result component */}
-                        </div>
-                    ))}
+                    <div>
+                        {dogDetails.map((dog, index) => (
+                            <div key={dog.id || index} className="dog-card">
+                                <img src={dog.img} alt={`${dog.name} picture`} />
+                                <p>Name: {dog.name}</p>
+                                <p>Age: {dog.age}</p>
+                                <p>Breed: {dog.breed}</p>
+                            </div>
+                        ))}
+                    </div>
                 </main>
             </div>
 
-            <div>
-                {dogDetails.map(dog => (
-                    <div key={dog.id}>
-                        <img src={dog.img} alt={dog.name} />
-                        <p>{dog.name}</p>
-                        <p>{dog.age}</p>
-                        <p>{dog.breed}</p>
-                    </div>
-                ))}
-            </div>
+
         </div>
     )
 }
